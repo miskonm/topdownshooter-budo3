1ec398e3d9c2df1419e18cae184882c2a9c60052
Nikita Misko <n.misko@wannaplay.gg>
2022-03-09 21:32:34 +0300

    Add cinemachine camera and delay shoot logic

--- Assets/Scripts/Game/Player/PlayerAttack.cs
+++ Assets/Scripts/Game/Player/PlayerAttack.cs
@@ -5,21 +5,37 @@ namespace TDS.Game.Player
     public class PlayerAttack : MonoBehaviour
     {
         [SerializeField] private PlayerAnimation _playerAnimation;
+
+        [SerializeField] private float _shootDelay = 0.5f;
         
         [SerializeField] private GameObject _bulletPrefab;
         [SerializeField] private Transform _bulletSpawnPointTransform;
 
+        private float _currentDelay;
+        
         private void Update()
         {
-            if (Input.GetButtonDown("Fire1"))
+            DecrementTimer(Time.deltaTime);
+
+            if (Input.GetButtonDown("Fire1") && CanShoot())
                 Attack();
         }
+
+        private void DecrementTimer(float deltaTime) =>
+            _currentDelay -= deltaTime;
+
+        private bool CanShoot() =>
+            _currentDelay <= 0f;
 
         private void Attack()
         {
             CreateBullet();
             _playerAnimation.PlayShoot();
+            SetDelay();
         }
+
+        private void SetDelay() =>
+            _currentDelay = _shootDelay;
 
         private void CreateBullet() =>
             Instantiate(_bulletPrefab, _bulletSpawnPointTransform.position, _bulletSpawnPointTransform.rotation);
