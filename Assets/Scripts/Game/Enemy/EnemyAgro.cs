3378dc6d04ad65e798d1d5efe8bfc013f2dae737
Nikita Misko <n.misko@wannaplay.gg>
2022-03-30 19:52:59 +0300

    Add enemy vision player logic

--- Assets/Scripts/Game/Enemy/EnemyAgro.cs
+++ Assets/Scripts/Game/Enemy/EnemyAgro.cs
@@ -7,20 +7,32 @@ namespace TDS.Game.Enemy
         [SerializeField] private TriggerObserver _triggerObserver;
         [SerializeField] private EnemyFollow _enemyFollow;
 
+        [Header("Raycast")]
+        [SerializeField] private LayerMask _raycastMask;
+
         private void Start()
         {
-            _triggerObserver.OnEntered += Entered;
             _triggerObserver.OnExited += Exited;
-        }
-
-        private void Entered(Collider2D obj)
-        {
-            _enemyFollow.enabled = true;
+            _triggerObserver.OnStayed += Stay;
         }
 
         private void Exited(Collider2D obj)
         {
             _enemyFollow.enabled = false;
+        }
+
+        private void Stay(Collider2D other)
+        {
+            if (_enemyFollow.enabled)
+                return;
+
+            Vector3 direction = other.transform.position - transform.position;
+            RaycastHit2D hit2D = Physics2D.Raycast(transform.position, direction, direction.magnitude, _raycastMask);
+
+            if (hit2D.collider != null)
+                return;
+
+            _enemyFollow.enabled = true;
         }
     }
 }
--- Assets/Scripts/Game/Enemy/TriggerObserver.cs
+++ Assets/Scripts/Game/Enemy/TriggerObserver.cs
@@ -7,9 +7,13 @@ namespace TDS.Game.Enemy
     {
         public event Action<Collider2D> OnEntered; 
         public event Action<Collider2D> OnExited; 
+        public event Action<Collider2D> OnStayed; 
 
         private void OnTriggerEnter2D(Collider2D other) =>
             OnEntered?.Invoke(other);
+
+        private void OnTriggerStay2D(Collider2D other) =>
+            OnStayed?.Invoke(other);
 
         private void OnTriggerExit2D(Collider2D other) =>
             OnExited?.Invoke(other);
