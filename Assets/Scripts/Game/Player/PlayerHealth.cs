b6a569ee924210a58719a98cd485092fa5fc07ea
Nikita Misko <n.misko@wannaplay.gg>
2022-03-30 21:10:00 +0300

    Refactor health

--- Assets/Scripts/Game/Player/PlayerHealth.cs
+++ Assets/Scripts/Game/Player/PlayerHealth.cs
@@ -7,38 +7,43 @@ namespace TDS.Game.Player
     public class PlayerHealth : MonoBehaviour, IHealth
     {
         [SerializeField] private int _maxHp;
-
         [SerializeField] private int _currentHp;
+        
         public event Action OnChanged;
+        
+        public int CurrentHp
+        {
+            get => _currentHp;
+            private set
+            {
+                int oldHp = _currentHp;
+                _currentHp = value;
 
-        public int CurrentHp { get; private set; }
+                _currentHp = Mathf.Max(_currentHp, MaxHp);
+
+                if (oldHp != _currentHp)
+                    OnChanged?.Invoke();
+            }
+        }
+
         public int MaxHp => _maxHp;
 
         private void Start()
         {
             CurrentHp = _maxHp;
-            _currentHp = CurrentHp;
         }
 
         public void ApplyDamage(int damage)
         {
             CurrentHp -= damage;
-            _currentHp = CurrentHp;
 
-            OnChanged?.Invoke();
-
-            // if (_currentHp < 0)
-            // TODO: _playerDeath.Die();
+            //if (CurrentHp < 0)
+            //TODO: _playerDeath.Die();
         }
 
         public void Heal(int healPoints)
         {
             CurrentHp += healPoints;
-            CurrentHp = Mathf.Max(CurrentHp, MaxHp);
-            _currentHp = CurrentHp;
-
-
-            OnChanged?.Invoke();
         }
     }
 }
